be7f01e43b9cbfe128dddd7d5580c9fed4c847d9
Raveen K Sharma <raveen08@gmail.com>
2020-05-11 23:45:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,12 +13,7 @@ end
 def monopoly
 	monopoly = {
 	  railroads: 
-	  { peices: 4, 
-	  rent_in_dollars: 
-	  one_piece_owned: 25,
-	  two_peices_owned: 50,
-	  three_pieces_owned: 100,
-    four_pieces_owned: 200
+	  { 
 	  },
 	  names: {
 	    reading_railroad:
